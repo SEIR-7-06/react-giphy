394be37fface68c2f6b00156a839dacd89cc730c
Anna Zocher <anna.zocher@gmail.com>
2021-06-22 16:55:04 -0700

    added boostrap

---
+++ src/components/SearchContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class SearchContainer extends Component {
+    render() {
+        return (
+            <div>
+                Hello!
+            </div>
+        )
+    }
+}
+
+export default SearchContainer
