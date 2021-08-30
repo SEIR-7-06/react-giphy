560c3bb85e0674b5bf42a27c5694d01a883dd979
off2mxrs <off2mxrs@gmail.com>
2021-08-30 13:26:32 -0700

    Search bar takes user input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,32 @@
-function Search() {
-    return (
-        <div>Search</div>
-    )
+import React from 'react'
+
+class Search extends React.Component {
+    state = {
+        inputVal: ''
+    }
+
+    //update inputVal to be whatever typed
+    handleChange = (event) => {
+        // event.target.value is the key(s) pressed
+        this.setState({inputVal: event.target.value })
+
+    }
+    
+    render() {
+
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text" 
+                        value={this.state.inputVal}
+                        onChange={this.handleChange}
+                    />
+                    <input type="submit" value="Search" />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Search
