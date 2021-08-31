4e8d7bd70c5077e623a5427064dc0316b2b56409
jekkiboi <hollymayer4@gmail.com>
2021-08-31 09:11:05 -0700

    my work

--- giphy/src/Home.jsx
+++ giphy/src/Home.jsx
@@ -7,7 +7,15 @@ console.log(API_KEY)
 function Home() {
   return (
     <div className="App">
+      <nav className='nav'>
+      <h3 className='srch'>Search</h3>
+      </nav>
+      <div className='title'>
       <h1>React Giphy</h1>
+      <h3 className='subtitle'>All them giphy thangz...</h3>
+      </div>
+      <br />
+      <br />
       <SearchContainer />
     </div>
   );
